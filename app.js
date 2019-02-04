b2d45ec065fa396e931abdfc6eaed41d61cf8e7f
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-02-04 15:23:10 -0500

    set up main server app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+// requiring stuff
+const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
+//create express server
+const app = express();
+
+// sets pot for herouku or local 3000
+const PORT = process.env.PORT || 3000;
+
+// setting up express app to handle data parsing
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+// sets up api routes files
+app.use(apiRoutes);
+app.use(htmlRoutes);
+
+app.listen (PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
