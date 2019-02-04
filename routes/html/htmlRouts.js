dc60bdb41fec896f65a43ca275b00cf545b0c010
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-02-04 15:53:33 -0500

    made html files w/ boilerplate. set up htmlRouts.js for public files.

---
+++ routes/html/htmlRouts.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const connection = require("../db/connection");
+
+// index html at /
+router.get("/", function(req, res){
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+// notes.html at /notes
+
+router.get("/notes", function(req, res){
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+// other paths get 404 page
+router.get("*", function(req, res) {
+  res.sendFile(path.join (--__dirname, "../public/404.html"));
+});
