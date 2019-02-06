5ce7be1a504a2d532092c1a86f5e1f40a7f0e277
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-02-05 22:29:10 -0500

    got ajax post working, and notes to automatically be added to note column without refreshing

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,20 +1,38 @@
 $( document ).ready(function() {
 let notes;
-$.ajax({
+
+function getNotes(){
+  $.ajax({
+        url: "/api/notes",
+        method: "GET"
+      }).then(function(response) {
+        notes = response;
+        console.log(notes);
+        $("#note-column").empty();
+        for (x of notes){
+          $(`<h4>${x.title}<h4><button id="delete-button"><i class="fas fa-dumpster"></i></button><p class ="hwText" >${x.body}</p>`).appendTo("#note-column");
+        };
+      });
+};
+
+getNotes();
+
+$("#save-note").on("click", function() {
+  event.preventDefault();
+  
+  const noteData = {
+      title: $("#title-input").val().trim(),
+      body: $("#text-input").val().trim()
+      
+    };
+    console.log(noteData);
+    $.ajax({
       url: "/api/notes",
-      method: "GET"
-    }).then(function(response) {
-      notes = response;
-      console.log(notes);
-      $("note-column").empty();
-      for (x of notes){
-        $(`<h4>${x.title}<h4><button id="delete-button"><i class="fas fa-dumpster"></i>
+      method: "POST",
+      data: noteData
+    }).then(getNotes());
+});
 
-</button><p class ="hwText" >${x.body}</p>`).appendTo("#note-column");
-      };
-    });
-
-// $("#save-note").on("click", 
 // $("#delete-button").on("click",
 
 
