9993410e5eab8c1458c8001f2bebfb5474bea81c
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-02-05 19:12:23 -0500

    front end ajax to put notes on page on page load.  implemented Google fonts.  fixed typos.

--- public/assets/index.js
+++ public/assets/index.js
@@ -4,9 +4,12 @@ $.ajax({
       url: "/api/notes",
       method: "GET"
     }).then(function(response) {
-      console.log(response);
       notes = response;
       console.log(notes);
+      $("note-column").empty();
+      for (x of notes){
+        $(`<h4>${x.title}<h4><p class ="hwText" >${x.body}</p>`).appendTo("#note-column");
+      };
     });
 
 
