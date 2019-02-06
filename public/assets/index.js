286194af0df77589d290a01996e1c5e5b6c3b247
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-02-05 21:34:51 -0500

    Set up delete buttons

--- public/assets/index.js
+++ public/assets/index.js
@@ -8,11 +8,14 @@ $.ajax({
       console.log(notes);
       $("note-column").empty();
       for (x of notes){
-        $(`<h4>${x.title}<h4><p class ="hwText" >${x.body}</p>`).appendTo("#note-column");
+        $(`<h4>${x.title}<h4><button id="delete-button"><i class="fas fa-dumpster"></i>
+
+</button><p class ="hwText" >${x.body}</p>`).appendTo("#note-column");
       };
     });
 
 // $("#save-note").on("click", 
+// $("#delete-button").on("click",
 
 
 
